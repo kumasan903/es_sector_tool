acedf988d886122d7f4b514048312dc9c08c075d
kumasan903 <kawanishi.souta.20050718@gmail.com>
2023-09-17 20:57:09 +0900

    dekitakamo

--- main.c
+++ main.c
@@ -39,7 +39,7 @@ static char*	delete_speace(char *src)
 
 	dst_i = 0;
 	src_i = 0;
-	dst = ft_calloc(ft_strlen_s(src), sizeof(char));
+	dst = ft_calloc(ft_strlen_s(src) + 1, sizeof(char));
 	while(src[src_i] == ' ')
 		src_i++;
 	while (src[src_i] != '\0')
@@ -47,7 +47,7 @@ static char*	delete_speace(char *src)
 		dst[dst_i] = src[src_i];
 		if (src[src_i] == ' ')
 		{
-			while (src[src_i + 1] == ' ')
+			while (src[src_i + 1] != '\0' && src[src_i + 1] == ' ')
 				src_i++;
 		}
 		dst_i++;
@@ -58,12 +58,37 @@ static char*	delete_speace(char *src)
 
 static void	write_wrapper(int output_fd, char *airport_code, char *rwy_name, char *sid_name, char *fixs)
 {
-//	printf("SID:%s:%s:%s:%s\n", airport_code, rwy_name, sid_name, delete_speace(fixs));
 	write(output_fd, airport_code, 4);
-	write(output_fd, rwy_name, 10);
-	write(output_fd, sid_name, 10);
-	write(output_fd, fixs, ft_strlen_s(fixs));
+	write(output_fd, ":", 1);
+	write(output_fd, rwy_name, ft_strlen_s(rwy_name));
+	write(output_fd, ":", 1);
+	write(output_fd, sid_name, ft_strlen_s(sid_name));
+	write(output_fd, ":", 1);
+	ft_putstr_fd(fixs, output_fd);
+//	write(output_fd, fixs, ft_strlen_s(fixs));
 	write(output_fd, "\n", 1);
+}
+
+void	all_rwy_star(int output_fd, char *airport_code, char *sid_name, char *fixs)
+{
+	size_t	i;
+	char	*rjah[] = {"03R", "03L", "21R", "21L", NULL};
+
+	if (starts_with(airport_code, "RJAH"))
+	{
+		i = 0;
+		while (rjah[i] != NULL)
+		{
+			write(output_fd, "STAR:", 5);
+			write(output_fd, airport_code, ft_strlen_s(airport_code));
+			write(output_fd, ":", 1);
+			write(output_fd, rjah[i], ft_strlen_s(rjah[i]));
+			write(output_fd, ":", 1);
+			write(output_fd, fixs, ft_strlen_s(fixs));
+			write(output_fd, "\n", 1);
+			i++;
+		}
+	}
 }
 
 int	main(void)
@@ -79,6 +104,7 @@ int	main(void)
 	char	**index_line;
 	char	*tmp;
 
+	printf("running......");
 	output_fd = open("./output.txt", O_WRONLY | O_TRUNC | O_CREAT, 0644);
 	if (output_fd == -1)
 	{
@@ -92,6 +118,7 @@ int	main(void)
 		printf("datファイルの読み込みに失敗しました。");
 		exit(1);
 	}
+	puts("test1");
 	while (1)
 	{
 		line = get_next_line(sid_fd);
@@ -100,17 +127,19 @@ int	main(void)
 		if(starts_with(line, "[RJ") || starts_with(line, "[RO"))
 		{
 			airport_code = ft_strndup(&(line[1]), 4);
-			rwy_name = ft_strndup(&(line[13]), ft_strclen_s(&(line[13]), ' '));
+			if (!starts_with(&(line[13]), "ALL"))
+				rwy_name = ft_strndup(&(line[15]), ft_strclen_s(&(line[15]), ' '));
+			else
+				rwy_name = ft_strndup(&(line[13]), ft_strclen_s(&(line[13]), ' '));
 			sid_name = ft_strndup(&(line[6]), ft_min(ft_strclen_s(&(line[6]), ' '), ft_strclen_s(&(line[6]), '/')));
 			while(1)
 			{
 				line = get_next_line(sid_fd);
-				if (line == NULL || line[0] == 13)
+				if (line == NULL || line[0] == 13 || line[0] == 10)
 					break;
 				fixs = ft_strjoin(ft_strjoin(fixs, ft_strndup(&(line[14]), 5)), " ");
 			}
-//			printf("SID:%s:%s:%s:%s\n", airport_code, rwy_name, sid_name, delete_speace(fixs));
-			write(output_fd, "SID", 3);
+			write(output_fd, "SID:", 4);
 			write_wrapper(output_fd, airport_code, rwy_name, sid_name, delete_speace(fixs));
 			free(fixs);
 			fixs = NULL;
@@ -124,18 +153,27 @@ int	main(void)
 		if(starts_with(line, "[RJ") || starts_with(line, "[RO"))
 		{
 			airport_code = ft_strndup(&(line[1]), 4);
-			rwy_name = ft_strndup(&(line[13]), ft_strclen_s(&(line[13]), ' '));
+			if (!starts_with(&(line[13]), "ALL"))
+				rwy_name = ft_strndup(&(line[15]), ft_strclen_s(&(line[15]), ' '));
+			else
+				rwy_name = ft_strndup(&(line[13]), ft_strclen_s(&(line[13]), ' '));
 			sid_name = ft_strndup(&(line[6]), ft_min(ft_strclen_s(&(line[6]), ' '), ft_strclen_s(&(line[6]), '/')));
 			while(1)
 			{
 				line = get_next_line(star_fd);
-				if (line == NULL || line[0] == 13)
+				if (line == NULL || line[0] == 13 || line[0] == 10)
 					break;
-				fixs = ft_strjoin(ft_strjoin(fixs, ft_strndup(&(line[13]), 5)), " ");
+				fixs = ft_strjoin(ft_strjoin(fixs, ft_strndup(&(line[14]), 5)), " ");
 			}
-//			printf("STAR:%s:%s:%s:%s\n", airport_code, rwy_name, sid_name, delete_speace(fixs));
-			write(output_fd, "STAR", 4);
-			write_wrapper(output_fd, airport_code, rwy_name, sid_name, delete_speace(fixs));
+			if (!starts_with(rwy_name, "ALL"))
+			{
+				write(output_fd, "STAR:", 5);
+				write_wrapper(output_fd, airport_code, rwy_name, sid_name, delete_speace(fixs));
+			}
+			else
+			{
+				all_rwy_star(output_fd, airport_code, sid_name, delete_speace(fixs));
+			}
 			free(fixs);
 			fixs = NULL;
 		}
